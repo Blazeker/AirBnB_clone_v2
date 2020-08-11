a020b7ed1e5c9fb0ba1149704056f282dd2bddc0
Blazeker <1687@holbertonschool.com>
2020-08-11 16:52:44 +0000

    Delete and all updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,11 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+              }
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
